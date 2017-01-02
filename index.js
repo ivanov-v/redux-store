82dd39772e7ea583952ee18cd10b950cc9be0ff6
Vadim Ivanov <sferamail33@yandex.ru>
2017-01-02 22:34:00 +0300

    Добавлена функция updateState

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let state = 0;
+
+function updateState(state, action) {
+  if (action === 'INCREMENT') {
+    return state + 1;
+  } else if (action === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state;
+  }
+}

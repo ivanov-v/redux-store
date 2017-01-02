806d0890d5d4bc8b08dd1530a2dd649c28436eef
Vadim Ivanov <sferamail33@yandex.ru>
2017-01-02 22:50:33 +0300

    Добавлены action

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 let state = 0;
 
 function updateState(state, action) {
-  if (action === 'INCREMENT') {
-    return state + 1;
-  } else if (action === 'DECREMENT') {
-    return state - 1;
+  if (action.type === 'INCREMENT') {
+    return state + action.amount;
+  } else if (action.type === 'DECREMENT') {
+    return state - action.amount;
   } else {
     return state;
   }
 }
+
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 4
+};
+
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 1
+};
+
+state = updateState(state, incrementAction);
+console.log(state);
+
+state = updateState(state, decrementAction);
+console.log(state);
+
+state = updateState(state, {});
+console.log(state);
